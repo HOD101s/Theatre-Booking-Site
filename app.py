92727c64ad7f06b8b803fec30ff1f6daab9e5980
HOD101s <manasbass.99@gmail.com>
2019-10-02 12:08:53 +0530

    Adds framework with ticket and home pages

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/ticket')
+def ticket():
+	return render_template('ticket.html')
+
+
+if __name__ == '__main__':
+	app.run()
